e6517014f70f000e1b2de9497f32f8f7d8d8e095
David Iruzubieta <david.iruzubieta@gmail.com>
2014-04-07 20:48:51 +0200

    Seguridad en acceso a funciones según el rol
    Ahora solo los administradores podrán acceder a determinadas partes de
    la aplicación. Más adelante se hará lo mismo para diferenciar el resto
    de roles.

--- app/routes.php
+++ app/routes.php
@@ -14,40 +14,50 @@ Route::group(array('before' => 'auth'), function()
     // Editamos un pedido concreto
     Route::get('pedido/{id}', 'PedidosController@detalles')
     ->where('id', '[0-9]+');
-    /*
-    	Route::get('/', function()
-	    {
-	        return View::make('home');
-	    });
-	*/
-    // Mostramos la pantalla de usuarios
-    Route::get('usuarios', 'UsuariosController@mostrarTodos');
-    Route::get('usuario/{id}', 'UsuariosController@usuario')
-    ->where('id', '[0-9]+');
-    Route::get('usuario/{id}/eliminar', 'UsuariosController@eliminar')
-    ->where('id', '[0-9]+');
-    Route::get('usuario/add', 'UsuariosController@formularioAdd');
+
+	Route::group(array('before' => 'rol'), function()
+	{
+	    // Mostramos la pantalla de usuarios
+	    Route::get('usuarios', 'UsuariosController@mostrarTodos');
+	    Route::get('usuario/{id}', 'UsuariosController@usuario')
+	    ->where('id', '[0-9]+');
+	    Route::get('usuario/{id}/eliminar', 'UsuariosController@eliminar')
+	    ->where('id', '[0-9]+');
+	    Route::get('usuario/add', 'UsuariosController@formularioAdd');
+	});
 
     Route::get('perfil', 'UsuariosController@perfil');
 
     // Cerramos la sesión
     Route::get('pruebas', 'HomeController@pruebas');
     Route::get('logout', 'AuthController@logout');
+
+    Route::group(array('before' => 'csrf'), function()
+	{
+		Route::group(array('before' => 'rol'), function()
+		{
+		    Route::post('usuario/{id}', 'UsuariosController@editar')
+		    ->where('id', '[0-9]+');
+		    Route::post('usuario/{id}/eliminar', 'UsuariosController@eliminar')
+		    ->where('id', '[0-9]+');
+		    Route::post('usuario/add', 'UsuariosController@add');
+		});
+		
+		Route::post('perfil', 'UsuariosController@editarPerfil');
+	});
 });
 
 Route::group(array('before' => 'csrf'), function()
 {
     // Validamos los datos de inicio de sesión
     Route::post('login', 'AuthController@postLogin');
+});
 
-    Route::post('usuario/{id}', 'UsuariosController@editar')
-    ->where('id', '[0-9]+');
-    Route::post('usuario/{id}/eliminar', 'UsuariosController@eliminar')
-    ->where('id', '[0-9]+');
-
-    Route::post('perfil', 'UsuariosController@editarPerfil');
-
-    Route::post('usuario/add', 'UsuariosController@add');
+Route::filter('rol', function()
+{
+	if(Auth::user()->rol_id != 1) {
+		return Redirect::to("/");
+	}
 });
 /*
 Route::get('albaranes', array('uses' => 'AlbaranesController@mostrarAlbaranes'));
